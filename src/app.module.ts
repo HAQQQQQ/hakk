8452cec41c9e5c6723bc3499e5f21925499272b4
kdab <kdabiedeen1@gmail.com>
2025-03-13 17:07:34 -0400

    db setup complete

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProfileModule } from './profile/profile.module';
+import { SupabaseService } from './supabase/supabase.service';
+import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [ProfileModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UsersController],
+  providers: [AppService, SupabaseService],
+  exports: [SupabaseService],
 })
 export class AppModule {}
