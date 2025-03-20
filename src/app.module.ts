41fb6f41d069c4c13cf2155922003e85b02e0647
kdab <kdabiedeen1@gmail.com>
2025-03-20 18:38:03 -0400

    add openai module with basic logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,8 @@ import { AppService } from "./app.service";
 import { ProfileModule } from "./profile/profile.module";
 import { SupabaseService } from "./supabase/supabase.service";
 import { UsersController } from "./users/users.controller";
-import { TestModule } from './test/test.module';
-import { OpenaiModule } from './openai/openai.module';
+import { TestModule } from "./test/test.module";
+import { OpenaiModule } from "./openai/openai.module";
 
 @Module({
 	imports: [ProfileModule, TestModule, OpenaiModule],
