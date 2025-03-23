f98898aed63db6aa836f53d69cc1ae0c3b2e063b
Kevin Setayesh <kevinsetayesh@gmail.com>
2025-03-22 23:36:37 -0400

    Changed default port for server from 3000 to 3001

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -7,18 +7,18 @@ import { ResponseInterceptor } from "./common/interceptors/response.interceptor"
 config();
 
 async function bootstrap() {
-	const port = process.env.PORT ?? 3000;
-	console.log(`---Server running on port: ${port}---`);
-	const app = await NestFactory.create(AppModule);
+    const port = process.env.PORT ?? 3001;
+    console.log(`---Server running on port: ${port}---`);
+    const app = await NestFactory.create(AppModule);
 
-	const configureGlobalMiddleware = () => {
-		app.useGlobalInterceptors(new ResponseInterceptor());
-		app.useGlobalFilters(new HttpExceptionFilter());
-	};
+    const configureGlobalMiddleware = () => {
+        app.useGlobalInterceptors(new ResponseInterceptor());
+        app.useGlobalFilters(new HttpExceptionFilter());
+    };
 
-	configureGlobalMiddleware();
+    configureGlobalMiddleware();
 
-	await app.listen(port);
+    await app.listen(port);
 }
 
 bootstrap();
