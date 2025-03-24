baad3fab9ac2d5eb2d63aada86fd287ee740b1ad
Kevin Setayesh <kevinsetayesh@gmail.com>
2025-03-23 23:13:16 -0400

    Added AppConfig for Redis service as well (prepping for docker setup)

--- packages/redis-service/src/main.ts
+++ packages/redis-service/src/main.ts
@@ -1,13 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { config } from 'dotenv';
-
-config();
+import { AppConfig } from "./config/app.config";
 
 async function bootstrap() {
-	const port = process.env.PORT ?? 3002;
-	console.log(`---Redis-server running on port: ${port}---`);
-	const app = await NestFactory.create(AppModule);
-	await app.listen(port);
+    const port = AppConfig.port;
+    console.log(`---Redis-server running on port: ${port}---`);
+    const app = await NestFactory.create(AppModule);
+    await app.listen(port);
 }
 bootstrap();
