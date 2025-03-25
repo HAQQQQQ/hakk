79771cefd93faafc02b69f92ea0fff559795ff77
Kevin Setayesh <kevinsetayesh7@gmail.com>
2025-03-25 09:53:08 -0400

    Fixed issue with dynamic port in default.conf.template

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Replace environment variables in the Nginx configuration template
-envsubst '$NGINX_HEADER_NAME $NGINX_SECURE_KEY $SERVER_PORT $CLIENT_PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '$NGINX_PORT $NGINX_HEADER_NAME $NGINX_SECURE_KEY $SERVER_PORT $CLIENT_PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Start Nginx in foreground mode
 nginx -g 'daemon off;'
