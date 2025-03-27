da111cc1229ea2ca7f2377cbf5aeae92fa072d2a
Kevin Setayesh <kevinsetayesh7@gmail.com>
2025-03-26 23:38:48 -0400

    Added flask api and the main server is able to connect to it

--- packages/server/src/modules/interests/interests.controller.ts
+++ packages/server/src/modules/interests/interests.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from "@nestjs/common";
 import { InterestsService } from "./interests.service";
+import axios from "axios";
 
 export interface InterestResponseDto {
 	id: number;
@@ -11,12 +12,28 @@ export class InterestsController {
 	constructor(private readonly interestsService: InterestsService) {}
 
 	@Get()
-	getAllInterests() {
+	async getAllInterests(): Promise<any[]> {
 		console.log("GET /api/interests route was hit"); // Log to confirm the route is being hit
+
+		await this.callPythonServer();
+
 		return [
 			{ id: 1, name: "Fitness" },
 			{ id: 2, name: "Music" },
 			{ id: 3, name: "Gaming" },
 		];
+	}
+
+	private async callPythonServer(): Promise<any> {
+		console.log("Before call to Python server");
+
+		try {
+			const res = await axios.get("http://localhost:5000/"); // Or POST, etc.
+			console.log("Response:", res.data);
+			return res.data;
+		} catch (err) {
+			console.error("Error calling Python server:", err.message);
+			return null;
+		}
 	}
 }
