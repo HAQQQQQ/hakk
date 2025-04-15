4d203f239db46ab7c84b803fe1cb0472093610e1
Kevin Setayesh <kevinsetayesh7@gmail.com>
2025-04-15 12:52:03 -0400

    Fixed module issue

--- packages/server/src/modules/transcriptions/transcriptions.module.ts
+++ packages/server/src/modules/transcriptions/transcriptions.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { TranscriptionsController } from "./transcriptions.controller";
 import { TranscriptionsService } from "./transcriptions.service";
 import { TranscriptionsRepository } from "./transcriptions.repository";
+import { SupabaseService } from "../supabase/supabase.service";
 
 @Module({
 	controllers: [TranscriptionsController],
-	providers: [TranscriptionsService, TranscriptionsRepository],
+	providers: [SupabaseService, TranscriptionsService, TranscriptionsRepository],
 })
 export class TranscriptionsModule {}
